f3cb3a07a752a6cf47ed2960b23aa27753bd0a5c
Sayam Pradhan <112542130+sayampradhan@users.noreply.github.com>
2023-11-13 18:23:17 +0530

    Create Program4.c

---
+++ Practicals/Program4.c
@@ -0,0 +1,28 @@
+// AIM - To find the sum of two numbers
+#include<stdio.h>
+
+
+// sum function
+void sum(int a, int b) {
+    // sum of two numbers
+    int sum = a + b;
+    printf("%d + %d = %d\n", a, b, sum);
+}
+
+
+// main function
+int main(){
+  int num1, num2;
+  
+  // taking input number 1
+  printf("num1: ");
+  scanf("%d", &num1);
+
+  // taking input number 2
+  printf("num2: ");
+  scanf("%d", &num2);
+
+  // calling sum function
+  sum(num1, num2);
+
+}
